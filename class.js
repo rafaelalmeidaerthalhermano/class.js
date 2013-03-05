6ef0247bd508c9caf7ecf19b991381eaf1f04616
Rafael Almeida Erthal Hermano <rafaerthal@gmail.com>
2013-03-05 03:01:47 -0300

    Update class.js

--- class.js
+++ class.js
@@ -23,169 +23,102 @@
  * @param constructor: função a ser executada
  */
 var Class = function (constructor) {
-    var _schema,
-        copyTo,
-        inherit,
-        extend,
-        ancestors = [],
-        parts = {},
-        that = this;
 
     if (!constructor || constructor.constructor !== Function) {
         throw 'Constructor must be a function';
     }
-
-    /* @function copyTo
-     *
-     * @author: Rafael Almeida Erthal Hermano
-     * @description: Adiciona novo métodos a algum objeto
-     * 
-     * @param container: objeto que receberá novos métodos
-     * @param parts: objeto que será inserido
-     */
-    copyTo = function (container, parts) {
-        var property
-        for (property in parts) {
-            if (
-                parts.hasOwnProperty(property) &&
-                property !== 'ubber'           &&
-                property !== 'instanceOf'      &&
-                property !== 'inherit'         &&
-                property !== 'extend'
-            ) {
-                container[property] = parts[property];
-            }
-        }
-    }
-
+    
     /* @function inherit
      *
      * @author: Rafael Almeida Erthal Hermano
      * @description: Adiciona nova classe antepassada
      * 
      * @param ancestor: a classe que será herdada
-     */
-    inherit = function (ancestor) {
-
-        if (ancestor.constructor.constructor !== Class) {
-            throw 'Ancestor must be a class';
+     */    
+    constructor.prototype.inherit = function (ancestor) {
+        var property,
+            temp = {};
+            
+        if (!this.ancestors) {
+            this.ancestors = [];
         }
-
-        ancestors.push(ancestor)
+        
+        ancestor.apply(temp , arguments.callee.caller.arguments);
+        this.ancestors.push(temp);
+        
+        for (property in temp) {
+            if (!this.hasOwnProperty(property)) {
+                this[property] = temp[property];
+            }
+        }
+        
+        for (property in ancestor.prototype) {
+            if (!this.hasOwnProperty(property)) {
+                this[property] = ancestor.prototype[property];
+            }
+        }
     };
 
     /* @function extend
      *
      * @author: Rafael Almeida Erthal Hermano
-     * @description: Adiciona novo métodos a classe
+     * @description: Adiciona novos atributos ao objeto
      * 
-     * @param properties: objeto que será inserido
-     */
-    extend = function (properties) {
-        copyTo(parts, properties);
-    };
+     * @param obj: atributos que serão inseridos
+     */    
+    constructor.prototype.extend = function (obj) {
+        var property;
 
-    _schema = function (params) {
-        var _class = {},
-            _instance = {},
+        for (property in obj) {
+            this[property] = obj[property];
+        }
+    };
+    
+    /* @function ubber
+     *
+     * @author: Rafael Almeida Erthal Hermano
+     * @description: Executa o método implementado no pai
+     */
+    constructor.prototype.ubber = function () {
+        var property,
             ancestor;
 
-        /* @function inherit
-         *
-         * @author: Rafael Almeida Erthal Hermano
-         * @description: Adiciona nova classe antepassada
-         * 
-         * @param ancestor: a classe que será herdada
-         */
-        _class.inherit = function (ancestor) {
-
-            if (ancestor.constructor.constructor !== Class) {
-                throw 'Ancestor must be a class';
-            }
-
-            inherit(ancestor);
-            copyTo(_class, new ancestor(params));
-        };
-
-        /* @function extend
-         *
-         * @author: Rafael Almeida Erthal Hermano
-         * @description: Adiciona novo métodos a classe
-         * 
-         * @param properties: objeto que será inserido
-         */
-        _class.extend = function (properties) {
-            extend(properties);
-            copyTo(_class, properties);
-        };
-
-        /* Métodos da instância */
-
-        /* @function ubber
-         *
-         * @author: Rafael Almeida Erthal Hermano
-         * @description: Executa o método implementado no pai
-         */
-        _instance.ubber = function () {
-            var caller,
-                i;
-            for (i in this) {
-                if (this[i] === arguments.callee.caller) {
-                    caller = i;
+        for (property in this) {
+            if (this[property] === arguments.callee.caller) {
+                for (ancestor in this.ancestors) {
+                    if (this.ancestors[ancestor].hasOwnProperty(property)) {
+                        return this.ancestors[ancestor][property].apply(this, arguments.callee.caller.arguments);
+                    }
                 }
             }
-            for (i in ancestors) {
-                if (ancestors[i]().hasOwnProperty(caller)) {
-                    return ancestors[i]()[caller].apply(this, arguments);
-                }
-            }
-        };
-
-        /* @function instanceOf
-         *
-         * @author: Rafael Almeida Erthal Hermano
-         * @description: Verifica se o objeto é da classe ancestor
-         * 
-         * @param ancestor: classe que será verificada
-         */
-        _instance.instanceOf = function (ancestor) {
-            var instanceOf = (ancestor.constructor === that);
-            for (var i in ancestors) {
-                if (ancestors[i].constructor === ancestor.constructor) {
-                    instanceOf = true;
-                }
-            }
-            return instanceOf;
-        };
-
-        /* @function extend
-         *
-         * @author: Rafael Almeida Erthal Hermano
-         * @description: Adiciona novo métodos ao objeto
-         * 
-         * @param properties: objeto que será inserido
-         */
-        _instance.extend = function (properties) {
-            copyTo(_instance, properties);
-        };
-
-        /* Colocando temporariamento os atributos na classe */
-        copyTo(_class, parts);
-        for (ancestor in ancestors) {
-            copyTo(_class, new ancestors[ancestor](params));
         }
-        constructor.apply(_class, [params]);
+    };
+    
+    /* @function instanceOf
+     *
+     * @author: Rafael Almeida Erthal Hermano
+     * @description: Verifica se o objeto é da classe ancestor
+     *
+     * @param obj: classe que será verificada
+     */
+    constructor.prototype.instanceOf = function (obj) {
+        return this.constructor === obj;
+    };
 
-        /* Passando os atributos para a instância */
-        copyTo(_instance, _class);
+    /* @function extend
+     *
+     * @author: Rafael Almeida Erthal Hermano
+     * @description: Adiciona novos atributos à classe
+     * 
+     * @param obj: atributos que serão inseridos
+     */    
+    constructor.extend = function (obj) {
+        var property;
 
-        return _instance;
-    }
-
-    /* Métodos da classe */
-    _schema.inherit = inherit;
-    _schema.extend = extend;
-    _schema.constructor = that;
-
-    return _schema;
+        for (property in obj) {
+            constructor.prototype[property] = obj[property];
+        }
+    };
+    
+    return constructor
 };
